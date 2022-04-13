95bbb0e3c9a1ed5ddab72ebd8b7050ac6618ff2b
Andres Cacchione <andres.cacchione@gmail.com>
2022-04-13 17:27:58 -0300

    test a) b) f)
    Agregado de test agregarElementoSimetriaTest()
    Agregado de test existenTodosLosElementoTest()
    Modificacion de test agregarElementoTest()

--- src/packageTest/MatrizAdyacenciaTest.java
+++ src/packageTest/MatrizAdyacenciaTest.java
@@ -21,16 +21,24 @@ public class MatrizAdyacenciaTest {
 		}
 	}
 	
+	
+	/*a. Crear un método llamado agregarElementoTest, que verifique que
+	luego de agregar un elemento este elemento exista dentro de la matriz*/
+	
 	@Test
 	public void agregarElementoTest() {
-		
-		for (int i = 0; i < tamanioMatriz; i++) {
-			for (int j = 0; j < tamanioMatriz; j++) {
-				matrizAdyacencia.agregarElemento(i, j);
-				assertTrue(matrizAdyacencia.existeElemento(i, j));
-			}
-		}
+		int posicionColuma=1;
+		int posicionFila=2;
+		matrizAdyacencia.agregarElemento(posicionFila, posicionColuma);
+		assertTrue(matrizAdyacencia.existeElemento(posicionFila, posicionColuma));
+		assertTrue(matrizAdyacencia.existeElemento(posicionColuma, posicionFila));
 	}
+	
+	
+	/*b. Crear un método llamado agregarElementoSimetriaTest, que
+	verifique que luego de agregar un elemento, ese exista en su posición
+	opuesta/simétrica. Ejemplo, si agrego un elemento en la posición [2,3],
+	verificar que se haya agregado el elemento [3,2]*/
 	
 	@Test
 	public void agregarElementoSimetriaTest() {
@@ -42,4 +50,21 @@ public class MatrizAdyacenciaTest {
 			}
 		}
 	}
+	
+	
+	/*f. Crear un método llamado existenTodosLosElementoTest Verificar que
+	si se completan todos las posiciones de la matriz, todos estos elementos
+	se hayan guardado correctamente en su posición original y en su
+	simetrico.*/
+	
+	@Test
+	public void existenTodosLosElementoTest() {		
+		for (int i = 0; i < tamanioMatriz; i++) {
+			for (int j = 0; j < tamanioMatriz; j++) {
+				matrizAdyacencia.agregarElemento(i, j);
+				assertTrue(matrizAdyacencia.existeElemento(i, j));
+			}
+		}
+	}
+	
 }
